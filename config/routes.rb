b4d690a3b1ab9c33c733980a828078285ed0af1d
Farooq Toppa <farooqtoppa@gmail.com>
2016-11-10 19:29:30 -0500

    added crud actions in users controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
  get '/', to: 'welcome#index'
- resources :users, only:[:create, :show, :destroy]
+ resources :users, only:[:create, :show]
  resources :books, only:[:create, :show, :update, :index, :destroy];
 end
