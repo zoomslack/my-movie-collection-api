4d46dc98bdd670164c34365b52f4dc01b1a0a1d3
Farooq Toppa <farooqtoppa@gmail.com>
2016-11-11 12:24:11 -0500

    fixed routes for books and added fake data to seed

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,53 @@
+class BooksController < ApplicationController
+
+  def index
+    books = Book.all
+    render json: {status: 200, books: books}
+  end
+
+  def show
+    book = Book.find(params[:id])
+    render json: {status: 200, book: book}
+  end
+
+  def create
+    user = User.find_by(name: book_params[:user_id])
+    book = Book.new(book_params)
+
+    book.user_id = user_id if user
+
+    if book.save
+      render json: {status: 200, book:book}
+    else
+      render json: {status: 422, book: book}
+    end
+  end
+
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    render json: {status: 200, book: book}
+  end
+
+  def destroy
+    book = Book.destroy(params[:id])
+    render json: {status: 204}
+  end
+
+  private
+  def book_params
+    params.required(:book).permit(
+      :title,
+      :author,
+      :publisher,
+      :publication_date,
+      :user_id
+    )
+  end
+end
+
+
+
+
+
+
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+10.times do
+  User.create(name: Faker::Internet.user_name)
+end
+
+10.times do
+  Book.create(
+    title: Faker::Hipster.sentence(3),
+    author: Faker::Team.name,
+    publisher: Faker::Team.name,
+    user_id: Faker::Number.between(1, 10)
+  )
+end
